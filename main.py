e76e0801942206d9828410dab848e8a982c5be9a
harishpesari <137114423+harishpesari@users.noreply.github.com>
2023-06-20 14:15:12 +0000

    Added the images desc and copyright bar at bottom

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app =Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
-def hello_world():
-  return "saikumar kakkerla"
+def index():
+	return render_template("base.html")
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',debug=True)
+	app.run(host='0.0.0.0',debug=True)
